48bb7d6878a909708d26d40d995734373bb18cf2
hayduke19us <hayduke19us@gmail.com>
2014-02-07 01:23:14 -0600

    updated the old_style.rb

--- lib/old_style.rb
+++ lib/old_style.rb
@@ -1,14 +1,5 @@
 $: << File.expand_path('../../lib', __FILE__)
-require 'load_dir'
-require 'format'
+require 'parse_dir'
 
-load_dir = LoadDir.new(ARGV)
-load_dir.html_directories
-load_dir.css_directories
-load_dir.segregate
-format = Format.new(found: load_dir.found_css, 
-                    empty: load_dir.empty_css,
-                    directories: load_dir.directories,
-                    html_files: load_dir.html,
-                    css_files: load_dir.css)
-format.write_index
+parse = ParseDir.new(ARGV)
+parse.write_index

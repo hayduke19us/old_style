2a63916df5961dc90158ba7ba839a77eb09dedb3
hayduke19us <hayduke19us@gmail.com>
2014-02-09 01:11:38 -0600

    test pass all is good

--- test/test_parse_dir.rb
+++ test/test_parse_dir.rb
@@ -46,16 +46,12 @@ class ParseDirTest < MiniTest::Test
     assert_equal 6, @parse.parse_css.keys.count
   end
 
-  def test_parse_hmtl_returns_collection_of_Nokogiri_Document_objects
-    assert_equal Nokogiri::HTML::Document, @parse.parse_html.first.class
-  end
-
   def test_parse_html_and_html_attribute_has_same_number_of_objects
     assert_equal @parse.html.count, @parse.parse_html.count
   end
 
   def test_found_css_finds_the_relevent_css
-    assert_equal 2, @parse.found.keys.count
+    assert_equal 3, @parse.found.keys.count
   end
 
   def test_empty_css_returns_a_hash_of_false_selectors
@@ -67,7 +63,7 @@ class ParseDirTest < MiniTest::Test
   end
 
   def test_good_percent
-    assert_equal "33.33%", @parse.good_percent
+    assert_equal "50.0%", @parse.good_percent
   end
 
   def test_success_message_if_Format#write_index_is_true
